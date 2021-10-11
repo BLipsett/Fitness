5449205bf73cfda61e7465bedce3252e6e3ffb05
Brian Lipsett <lipsettbrian3@gmail.com>
2021-10-11 14:11:05 -0600

    add appbar mui

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Homepage from './pages/homepage/homepage.component';
+import Header from './components/header/header.component';
 
 
 class App extends React.Component {
@@ -9,9 +10,12 @@ class App extends React.Component {
   render() {
     return (
       <Router>
+        <div>
+        <Header />
         <Route exact path='/'>
         <Homepage />
         </Route>
+        </div>
       </Router>
       );
     }
