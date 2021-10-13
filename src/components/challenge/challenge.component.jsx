52da713846460084acf81e7fc501fea57e0c1edc
Brian Lipsett <lipsettbrian3@gmail.com>
2021-10-12 19:51:20 -0600

    challenge mins

--- src/components/challenge/challenge.component.jsx
+++ src/components/challenge/challenge.component.jsx
@@ -1,13 +1,43 @@
-import { render } from '@testing-library/react'
 import React from 'react'
-
 import './challenge.styles.scss'
 
 class Challenge extends React.Component {
+constructor(props){
+  super(props)
+
+  this.state = {
+      currentScore: ''
+  }
+
+}
+
+handleSubmit = async event => {
+  event.preventDefault()
+  const { currentScore } = this.state
+}
+
+handleChange = async event => {
+  const {name, value} = event.target;
+  this.setState({[name]: value})
+}
   render() {
+    const { currentScore } = this.state
     return (
       <div className='challenge'>
-        <h1>Walk 480 minutes total</h1>
+        <h1>Walk 900 minutes total</h1>
+        <p>enter your total minutes</p>
+        <form onSubmit={this.handleSubmit}>
+
+        <input
+        type='number'
+        name='current-score'
+        label='current-score'
+        onChange={this.handleChange}
+        // value={currentScore} required
+        ></input>
+        <button type='submit'>add mins</button>
+        </form>
+        <p>current minutes: {currentScore}</p>
       </div>
 )
 }
