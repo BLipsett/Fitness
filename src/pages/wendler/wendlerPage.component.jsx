6074cb4f4b2c3cd404c48988fa30f8da35a17c0e
Brian Lipsett <lipsettbrian3@gmail.com>
2021-10-25 16:30:57 -0600

    wendler page

--- src/pages/wendler/wendlerPage.component.jsx
+++ src/pages/wendler/wendlerPage.component.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import './wendlerPage.styles.scss'
 
 class WendlerPage extends React.Component {
  
 
+  
+addMaxes() {
+  alert(
+    'What is your 1 rep max Bench'
+  )
+  console.log('add new')
+}
   render() {
     return (
       <div className="wendler-page">
         <h1>wendler workout chart</h1>
+        <button onClick={() => this.addMaxes()}>Add One Rep Maxes</button>
+        <ul>
+        <li>
+        <h3>week 1</h3>
+        </li>
+        <li>
+        <h3>week 2</h3>
+        </li>
+        <li>
+        <h3>week 3</h3>
+        </li>
+        <li>
+        <h3>week 4</h3>
+        </li>
+        </ul>
       </div>
     )
   }
