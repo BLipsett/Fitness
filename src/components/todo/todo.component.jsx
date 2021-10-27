7fbe9ea607e7b2c2f5a01cec7101c16285a7f2b1
Brian Lipsett <lipsettbrian3@gmail.com>
2021-10-27 15:45:55 -0600

    persusting todos

--- src/components/todo/todo.component.jsx
+++ src/components/todo/todo.component.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function Todo({todo}) {
+export default function Todo({todo, toggleTodo}) {
+  function handleTodo() {
+    toggleTodo(todo.id)
+  }
   return (
     <div>
+      <label>
+      <input type="checkbox" checked={todo.isComplete} onChange={handleTodo}></input>
       <h3>
       {todo.name}
       </h3>
+      </label>
     </div>
   )
 }
--- src/pages/todoPage/todoPage.component.jsx
+++ src/pages/todoPage/todoPage.component.jsx
@@ -1,19 +1,67 @@
-import React, { useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import TodoList from '../../components/todo/todoList.component'
 import './todoList.styles.scss'
+import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoPage() {
 
   //use state hook returns an array
-  const [todos, setTodos] = useState([{id: 1, name: 'todo 1', isComplete: false}])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  const LOCAL_STORAGE_KEY = 'todos.app'
+
+  
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if(storedTodos) {
+      setTodos(storedTodos)
+    }
+  }, [])
+  
+  useEffect(() => {
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+
+    todo.isComplete = !todo.isComplete;
+    setTodos(newTodos)
+  }
+
+
+  function handleAddTodo(e) {
+  const name = todoNameRef.current.value
+
+  if(name === '' ) {
+    alert('must add name')
+  }
+  console.log(name)
+  setTodos(prevTodos => {
+    return [...prevTodos, { id: uuidv4(), name: name, isComplete: false}]
+  })
+  
+  todoNameRef.current.vaulue = null
+}
+
+function clearTodos() {
+
+  const newTodos = todos.filter(todo => todo.isComplete === false)
+  setTodos(newTodos)
+}
   
   return (
     <div className='todo-main'>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <h1>To-Do List Page</h1>
-      <input type="text" />
-      <button> Add Todo</button>
-      <button>Clear Complete</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}> Add Todo</button>
+      <button onClick={clearTodos}>Clear Complete</button>
+      <div>
+        {todos.filter(todo => !todo.isComplete).length} left to do
+      </div>
     </div>
   )
 }
